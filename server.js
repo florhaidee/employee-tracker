1ea1646025cf198d7a1aab57bee04c3938554707
Florhaidee <florhaideedev@gmail.com>
2020-07-27 01:26:07 -0600

    created function to add new role

--- server.js
+++ server.js
@@ -6,5 +6,5 @@ const {getAlldepartments, addDepartment} = require('./utils/departments')
 const {getAllRoles, addRole} = require('./utils/roles')
 
 //addDepartment('testing');
-getAllRoles();
-//addRole({name: 'Intern', salary: 80000, department_id: 2});
+// getAllRoles();
+addRole('Intern', 80000, 2);
